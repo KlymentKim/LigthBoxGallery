f03acab4dc37fd118b0f783c3066d5a4e8700309
Klyment <49413026+KlymentKim@users.noreply.github.com>
2023-03-27 01:20:09 +0300

    Add files via upload

---
+++ js/01-gallery.js
@@ -0,0 +1,72 @@
+import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
+// Change code below this line
+
+// Створити функцію для рендерингу розмітки на основі масиву даних galleryItems 
+// та шаблону елемента галереї.Функція повинна додати розмітку до елементу з класом gallery на сторінці.
+
+const gallery = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+  return items.reduce((acc, { preview, original, description }) => {
+    return acc + `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+          <img
+            class="gallery-image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  },'');
+}
+// Add gallery items to the DOM
+gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+gallery.addEventListener('click', onGalleryContainerClick);
+
+//check for image 
+function onGalleryContainerClick(event) {
+     event.preventDefault();
+       
+    const isGalleryImage = event.target;
+    if (isGalleryImage.nodeName !== 'IMG' || !isGalleryImage.classList.contains('gallery-image')) {
+        return;
+    }
+    
+  const imageSet = event.target.dataset.source;
+  
+    // відкриття картинки на повний єкран бібліотека Lightbox
+  const instance = basicLightbox.create(`
+    <img src="${imageSet}" alt="${isGalleryImage.alt}" />`, {
+  onShow: (instance) => {
+    const image = instance.element().querySelectorAll('img');
+    image.onload = () => {
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+      const imageWidth = image.width;
+      const imageHeight = image.height;
+      const leftOffset = (windowWidth - imageWidth)/2;
+      const topOffset = (windowHeight - imageHeight)/2;
+      image.style.left = leftOffset + 'px';
+      image.style.top = topOffset + 'px';
+    };
+    }
+      
+  });
+  instance.show();
+
+    // Close modal window on Escape key press
+  document.addEventListener("keydown", (event) => {
+        if (event.key == 27 || event.key === 'Escape' || event.visible())
+      instance.close();
+  });
+}
+  
+
+   //   const instance = basicLightbox.create(`
+  //   <img src="${imageSet}" alt="${isGalleryImage.alt}" width="1200" height="800">
+  //   `);
---
+++ js/gallery-items.js
@@ -0,0 +1,87 @@
+export const galleryItems = [
+    {
+      preview:
+        'https://i.postimg.cc/yYYFJ2PD/1-480px.jpg',
+      original:
+        'https://i.postimg.cc/Hx3v1Xzn/1-1280px.jpg',
+      description: ' Skhidnytsya',
+    },
+    {
+      preview:
+        'https://i.postimg.cc/VkNWPVvc/2-480px.jpg',
+      original:
+        'https://i.postimg.cc/Kz39Ss9Y/2-1280px.jpg',
+      description: 'Container Haulage Freight',
+    },
+    {
+      preview:
+        'https://i.postimg.cc/QMQPnMMM/2-480px.jpg',
+      original:
+        'https://i.postimg.cc/SR1N2d0H/2-1280px.jpg',
+      description: 'Aerial Beach View',
+    },
+    {
+      preview:
+        'https://i.postimg.cc/rsp0dzHZ/2-480px.jpg',
+      original:
+        'https://i.postimg.cc/k43LXb29/2-1280px.jpg',
+      description: 'Flower Blooms',
+    },
+    {
+      preview:
+        'https://i.postimg.cc/BbF1xtsq/2-480px.jpg',
+      original:
+        'https://i.postimg.cc/3NzHmJ1F/2-1280px.jpg',
+      description: 'Alpine Mountains',
+    },
+    {
+      preview:
+        'https://i.postimg.cc/vBWHtKDV/2-480px.jpg',
+      original:
+        'https://i.postimg.cc/L554F8Nz/2-1280px.jpg',
+      description: 'Mountain Lake Sailing',
+    },
+    {
+      preview:
+        'https://i.postimg.cc/3R9HtQjz/2-480px.jpg',
+      original:
+        'https://i.postimg.cc/XqG4qDFh/2-1280px.jpg',
+      description: 'Alpine Spring Meadows',
+    },
+    {
+      preview:
+        'https://i.postimg.cc/TYjV2rvd/2-480px.jpg',
+      original:
+        'https://i.postimg.cc/PxZ4m99Z/2-1280px.jpg',
+      description: 'Nature Landscape',
+    },
+    {
+      preview:
+        'https://i.postimg.cc/1znJJf2L/2-480px.jpg',
+      original:
+        'https://i.postimg.cc/NjtJGsyX/2-1280px.jpg',
+      description: 'Lighthouse Coast Sea',
+    },
+    {
+      preview:
+        'https://i.postimg.cc/02CXVV0m/2-480px.jpg',
+      original:
+        'https://i.postimg.cc/JhK1m38B/2-1280px.jpg',
+      description: 'Lighthouse Coast Sea',
+    },
+    {
+      preview:
+        'https://i.postimg.cc/R09MkS3h/2-480px.jpg',
+      original:
+        'https://i.postimg.cc/zfF8FsGh/2-1280px.jpg',
+      description: 'Lighthouse Coast Sea',
+    },
+    {
+      preview:
+        'https://i.postimg.cc/yxkgDmzk/2-480px.jpg',
+      original:
+        'https://i.postimg.cc/9fLnJ5rR/2-1280px.jpg',
+      description: 'Lighthouse Coast Sea',
+    },
+  ];
+  
